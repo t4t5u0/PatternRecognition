b6002dfb36e5cce78f3cf7355a11b8eebde17725
kota-shiokara <ikanoshiokara.fun@gmail.com>
2021-07-24 16:17:46 +0900

    fix: change tmp into result

--- faceArea.py
+++ faceArea.py
@@ -14,7 +14,7 @@ cascade_path = "./model/haarcascade_frontalface_default.xml"
 # 顔領域をtupleで返す(x, y, w, h)
 def faceArea(img: np.ndarray) -> tuple:
     tmpImg = img
-    tmp = None
+    result = None
     # 分類機の特徴量を取得
     cascade = cv2.CascadeClassifier(cascade_path)
     # 顔領域を取得
@@ -22,10 +22,10 @@ def faceArea(img: np.ndarray) -> tuple:
     # 取得した顔領域からx, y, w, hを取って黒、太さ2の四角形を書き込む
     for x, y, w, h in faceareas:
         cv2.rectangle(tmpImg, (x, y), (x + w, y + h), 0, thickness=2)
-        tmp = (x, y, w, h)
+        result = (x, y, w, h)
     cv2.imwrite('img/output.png', tmpImg)
-    if(tmp != None):
-        return tmp
+    if(result != None):
+        return result
     else:
         return (0, 0, 0, 0)
 
