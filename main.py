48a942a95f12bc771086b415aeee1cd715e45faa
t4t5u0 <ymmtryk0902@gmail.com>
2021-07-27 13:38:17 +0900

    fix: remove redundant code

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from norm import *
 def main():
     path = './img/tsuru512006_TP_V4.jpg'
     # norm64(clip(*faceArea(getGrayImage(path))))
-    face = FaceArea(path)
-    norm_img_path = face.getGrayImage().faceArea().clip().norm64().norm_img_path
-    norm_img = face.getGrayImage().faceArea().clip().norm64().norm_img
+    face = FaceArea(path).getGrayImage().faceArea().clip().norm64()
+    norm_img_path = face.norm_img_path
+    norm_img = face.norm_img
 
 
 if __name__ == '__main__':
